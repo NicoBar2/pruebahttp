59c9796ab4c23718a008745eb15fbcb82587af8c
NicoBar2 <nicoleyro13@gmail.com>
2024-02-15 19:39:28 -0500

    LOCAL

---
+++ android/app/src/main/kotlin/com/example/pruebahttp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pruebahttp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HTTP Request Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<dynamic> _posts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPosts();
+  }
+
+  Future<void> _fetchPosts() async {
+    try {
+      final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+      if (response.statusCode == 200) {
+        setState(() {
+          _posts = json.decode(response.body);
+        });
+      } else {
+        throw Exception('Failed to load posts');
+      }
+    } catch (error) {
+      print('Error fetching posts: $error');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Posts'),
+      ),
+      body: _posts.isEmpty
+          ? Center(
+        child: CircularProgressIndicator(),
+      )
+          : ListView.builder(
+        itemCount: _posts.length > 5 ? 5 : _posts.length, // Máximo 5 elementos
+        itemBuilder: (context, index) {
+          final post = _posts[index];
+          return ListTile(
+            title: Text(post['title']),
+            subtitle: Text(post['body']),
+          );
+        },
+      ),
+    );
+  }
+}
+
